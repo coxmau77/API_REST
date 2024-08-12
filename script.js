7004fe5998a29dbf94d5c9b9f3353d265bd3bf71
Mauricio Cox <coxmau77@gmail.com>
2024-08-12 12:35:37 -0300

    chore: Endpoints y query parameters

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const API_URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=10";
 
 async function reload() {
   const response = await fetch(API_URL);
   const data = await response.json();
-  const img = document.querySelector("img");
-  img.src = data[0].url;
+  const section = document.querySelector("section");
+  section.innerHTML = "";
+
+  for (let image in data) {
+    section.innerHTML += `
+        <figure>
+            <img src=${data[image].url} alt="Descripción de la imagen">
+            <figcaption>Image ID:<mark> ${data[image].id} </mark></figcaption>
+        </figure>
+    `;
+  }
 }
 
-reload(); // Carga una imagen en cuanto se ejecuta el script
+reload(); // Carga 10 imagenes en cuanto se ejecuta el script
