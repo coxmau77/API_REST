d08d1c5fca27cebaa567df54598d7cd04fb45c59
Mauricio Cox <coxmau77@gmail.com>
2024-08-12 11:48:21 -0300

    chore: implementacion de async await

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const URL = "https://api.thecatapi.com/v1/images/search";
-fetch(URL)
-  .then((response) => response.json())
-  .then((data) => {
-    const img = document.querySelector("img");
-    img.src = data[0].url;
-  });
+const API_URL = "https://api.thecatapi.com/v1/images/search";
+
+async function reload() {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  const img = document.querySelector("img");
+  img.src = data[0].url;
+}
+
+reload(); // Carga una imagen en cuanto se ejecuta el script
